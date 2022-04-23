8832af2cf24dfc7da06ec2dd7bc0bc6be95d35f7
Dewkong <kryniu113@gmail.com>
2022-04-23 17:55:19 +0200

    [RAP-39-start-fight-view] removed unnecessary styles in Fight.svelte

--- client/src/routes/Fight.svelte
+++ client/src/routes/Fight.svelte
@@ -8,16 +8,5 @@
 
 
 <style>
-    main {
-        text-align: center;
-        padding: 1em;
-        margin: 0 auto;
-    }
 
-
-    @media (min-width: 640px) {
-        main {
-            max-width: none;
-        }
-    }
 </style>
