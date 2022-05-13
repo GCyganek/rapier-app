c92992ddf5fac9f63de39ae0d16ffa02a0eb4319
Gabriel Cyganek <gabrielcyganek@gmail.com>
2022-05-13 17:21:17 +0200

    [RAP-94] linter and prettier added with additional npm scripts, code formatted

--- client/src/routes/fight/fight-sequence-components/Actions.ts
+++ client/src/routes/fight/fight-sequence-components/Actions.ts
@@ -1,28 +1,33 @@
 export enum Actions {
-    RED, BLUE,
-    ATTACK, COUNTER_ATTACK,
-    SUCCESS, FAILURE,
-    TIMING, LATE,
-    HEAD, BODY, HAND,
-};
-
-export namespace Actions {
-    const actionMap = {
-        [Actions.RED]:      "Czerwony",
-        [Actions.BLUE]:     "Niebieski",
-        [Actions.ATTACK]:   "Atak",
-        [Actions.COUNTER_ATTACK]: "Kontratak",
-        [Actions.SUCCESS]:  "Udany",
-        [Actions.FAILURE]:  "Nieudany",
-        [Actions.TIMING]:   "W Tempo",
-        [Actions.LATE]:     "Spóźniony",
-        [Actions.HEAD]:     "W Głowę",
-        [Actions.BODY]:     "W Ciało",
-        [Actions.HAND]:     "W Rękę"
-    };
-
-    export function toHumanReadable(action: Actions) {
-        return actionMap[action];
-    }
+  RED,
+  BLUE,
+  ATTACK,
+  COUNTER_ATTACK,
+  SUCCESS,
+  FAILURE,
+  TIMING,
+  LATE,
+  HEAD,
+  BODY,
+  HAND,
 }
 
+export namespace Actions {
+  const actionMap = {
+    [Actions.RED]: 'Czerwony',
+    [Actions.BLUE]: 'Niebieski',
+    [Actions.ATTACK]: 'Atak',
+    [Actions.COUNTER_ATTACK]: 'Kontratak',
+    [Actions.SUCCESS]: 'Udany',
+    [Actions.FAILURE]: 'Nieudany',
+    [Actions.TIMING]: 'W Tempo',
+    [Actions.LATE]: 'Spóźniony',
+    [Actions.HEAD]: 'W Głowę',
+    [Actions.BODY]: 'W Ciało',
+    [Actions.HAND]: 'W Rękę',
+  };
+
+  export function toHumanReadable(action: Actions) {
+    return actionMap[action];
+  }
+}
