9179c0847a7185d3db01db77b280d2f07063ba0d
karpinsk <karpinskiy@student.agh.edu.pl>
2022-05-16 20:21:04 +0200

    RAP-88

--- client/src/routes/fight/fight-sequence-components/Actions.ts
+++ client/src/routes/fight/fight-sequence-components/Actions.ts
@@ -1,35 +1,35 @@
 export enum Actions {
-    RED, 
-    BLUE, 
-    DRAW,
-    ATTACK, 
-    COUNTER_ATTACK,
-    SUCCESS, 
-    FAILURE,
-    TIMING, 
-    LATE,
-    HEAD, 
-    BODY, 
-    HAND
-};
+  RED,
+  BLUE,
+  DRAW,
+  ATTACK,
+  COUNTER_ATTACK,
+  SUCCESS,
+  FAILURE,
+  TIMING,
+  LATE,
+  HEAD,
+  BODY,
+  HAND,
+}
 
 export namespace Actions {
-    const actionMap = {
-        [Actions.RED]:      "Czerwony",
-        [Actions.BLUE]:     "Niebieski",
-        [Actions.DRAW]:     "Remis",
-        [Actions.ATTACK]:   "Atak",
-        [Actions.COUNTER_ATTACK]: "Kontratak",
-        [Actions.SUCCESS]:  "Udany",
-        [Actions.FAILURE]:  "Nieudany",
-        [Actions.TIMING]:   "W Tempo",
-        [Actions.LATE]:     "Spóźniony",
-        [Actions.HEAD]:     "W Głowę",
-        [Actions.BODY]:     "W Ciało",
-        [Actions.HAND]:     "W Rękę"
-    };
+  const actionMap = {
+    [Actions.RED]: 'Czerwony',
+    [Actions.BLUE]: 'Niebieski',
+    [Actions.DRAW]: 'Remis',
+    [Actions.ATTACK]: 'Atak',
+    [Actions.COUNTER_ATTACK]: 'Kontratak',
+    [Actions.SUCCESS]: 'Udany',
+    [Actions.FAILURE]: 'Nieudany',
+    [Actions.TIMING]: 'W Tempo',
+    [Actions.LATE]: 'Spóźniony',
+    [Actions.HEAD]: 'W Głowę',
+    [Actions.BODY]: 'W Ciało',
+    [Actions.HAND]: 'W Rękę',
+  };
 
-    export function toHumanReadable(action: Actions) {
-        return actionMap[action];
-    }
+  export function toHumanReadable(action: Actions) {
+    return actionMap[action];
+  }
 }
