925308fca9e05cd9ac9f9d170807a288360869b4
robertkbk <33332309+robertkbk@users.noreply.github.com>
2022-05-21 12:05:12 +0200

    Proposition details

--- client/src/routes/fight/FightSideProposition.svelte
+++ client/src/routes/fight/FightSideProposition.svelte
@@ -1,19 +1,32 @@
-<script>
-    import Button, {Label} from "@smui/button";
+<script lang="ts">
+    import type { Response } from 'model/Communication';
+    import Button, { Label } from "@smui/button";
+    import { FightSocket, key } from "routes/FightSocket";
+    import { getContext } from "svelte";
+import FightPropositionDetails from './FightPropositionDetails.svelte';
 
-    function handleClick(type){
+    const socket = getContext(key) as FightSocket;
+
+    const [red, blue] = socket.awaitSuggestions();
+
+    function handleClick(type: Response.Color) {
         console.log("clicked! " + type)
     }
 </script>
 
 <div class="sideDiv">
-    <p>Proponowane zdarzenia od sędziów bocznych:</p>
-    <Button class="redButton" disabled=true on:click={() => handleClick("red")}>
-        <Label>[informacje od czerwonego]</Label>
+    <p> Proponowane zdarzenia od sędziów bocznych: </p>
+    
+    <FightPropositionDetails suggestion={$red} />
+    <FightPropositionDetails suggestion={$blue} />
+    <!-- 
+    <Button class="redButton" disabled={true} on:click={() => handleClick("RED")}>
+        <Label> {$red} </Label>
     </Button>
-    <Button class="blueButton" on:click={() => handleClick("blue")}>
+    
+    <Button class="blueButton" on:click={() => handleClick("BLUE")}>
         <Label>[informacje od niebieskiego]</Label>
-    </Button>
+    </Button> -->
 </div>
 
 <style>
