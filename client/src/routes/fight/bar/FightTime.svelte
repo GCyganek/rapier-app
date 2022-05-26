f3f93d24b528a7b054b4f5cd9d45b3e74c2f0b49
karpinsk <karpinskiy@student.agh.edu.pl>
2022-05-26 15:21:13 +0200

    Fixed according to comments

--- client/src/routes/fight/bar/FightTime.svelte
+++ client/src/routes/fight/bar/FightTime.svelte
@@ -3,12 +3,18 @@
 </script>
 
 <script lang="ts">
+<<<<<<< HEAD
   import { createEventDispatcher, getContext, onMount } from 'svelte';
   import { emptySeq } from '../fight-sequence-components/Store';
   import Icon from '@iconify/svelte';
   import FightEnd from '../modal/FightEnd.svelte';
   import type { Response } from 'model/Communication';
   import { Events, FightSocket, key } from 'routes/FightSocket';
+=======
+    import { createEventDispatcher } from 'svelte';
+    import { emptySeq } from '../fight-sequence-components/Store';
+    import Icon from '@iconify/svelte';
+>>>>>>> 5114ab9 (Fixed according to comments)
 
   let time: number = 0;
   let timeout: NodeJS.Timeout = null;
@@ -75,9 +81,9 @@
 </script>
 
 <div>
-  <button class="returnButton" on:click={() => dispatch('return')} hidden={#emptySeq}>
-    <Icon icon="bx:left-arrow-circle" color="#2f4858" height="2rem" />
-  </button>
+    <button class="returnButton" on:click={() => dispatch('return')} hidden={$emptySeq}>
+        <Icon icon="bx:left-arrow-circle" color="#2f4858" height="2rem"/>
+    </button>
 
   <div class="info">
     {showTime(time)}
