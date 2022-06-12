3d3ac762bd4920fbd0aa1490ef28ab8fbea7a590
Gabriel Cyganek <gabrielcyganek@gmail.com>
2022-06-12 13:04:44 +0200

    [release/1.0.0] another cors options modification...

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,7 +4,14 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const options = {
-    origin: 'http://localhost:8080',
+    origin: [
+      'http://localhost:8080',
+      'https://62a5bcee8ab5b100099707af--sweet-kitsune-4698d9.netlify.app/',
+    ],
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,PUT,POST,DELETE,UPDATE,OPTIONS',
+    credentials: true,
   };
   app.enableCors(options);
   await app.listen(process.env.PORT || 3000);
