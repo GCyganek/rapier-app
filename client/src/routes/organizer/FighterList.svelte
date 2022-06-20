0d0ef27a6b14381f4a67ea281ac072e40fc05444
Dewkong <kryniu113@gmail.com>
2022-06-20 21:36:53 +0200

    added two organizer screens: player list and add player form

---
+++ client/src/routes/organizer/FighterList.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  const getPlayers = async () => {
+    const response = await fetch('api/get-players');
+    return await response.json();
+  };
+</script>
+
+{#await getPlayers()}
+  <p>...waiting</p>
+{:then players}
+  <div class="background">
+    {#each players as player}
+      <div class="player">{player.firstName} {player.lastName}</div>
+    {/each}
+  </div>
+{:catch error}
+  <p>An error occurred! {error}</p>
+{/await}
+
+<style>
+  div.background {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background-image: url('../resources/fightstart_background_semitransparent.png');
+    background-repeat: repeat;
+  }
+  div.player {
+    font-size: 2rem;
+    font-weight: bold;
+    background-color: white;
+    border: 0.2rem solid black;
+    padding: 0.3rem;
+    margin: 0.3rem;
+  }
+  p {
+    font-size: 2rem;
+    font-weight: bold;
+    -webkit-text-stroke: 0.05rem white;
+    text-align: center;
+  }
+</style>
---
+++ client/src/routes/organizer/Organizer.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { push } from 'svelte-spa-router';
+
+  function navigateFighterList() {
+    push('/fighterlist');
+  }
+
+  function navigateAddFighter() {
+    push('/addfighter');
+  }
+</script>
+
+<div class="organizer">
+  <button on:click={navigateFighterList}> Lista zawodników </button>
+  <button on:click={navigateAddFighter}> Dodaj zawodnika </button>
+</div>
+
+<style>
+  div.organizer {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+    background-image: url('../resources/fightstart_background_semitransparent.png');
+    background-repeat: repeat;
+  }
+
+  button {
+    border: 1px black solid;
+    font-size: 2rem;
+  }
+</style>
